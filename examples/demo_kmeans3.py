26e0af70945719242a2830a55e92d329bc86ded6
V SS Karthik <vsskartheek@gmail.com>
2026-03-30 14:41:41 +0530

    Refactor K-Means demo to use new imports

--- examples/demo_kmeans3.py
+++ examples/demo_kmeans3.py
@@ -1,13 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys, os
 
-# Route Python to your GlassBoxML root directory
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from datasets.generators import make_circles
-# Import your v2 K-Means!
-from models.kmeans2 import KMeansClustering # (Adjust import name if necessary)
+from glassboxml.datasets import make_circles
+from glassboxml.models import KMeansClustering 
 
 def main():
     print("==================================================")
