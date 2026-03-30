a0c4a225ff151b685d1a0ef26047355381262c23
V SS Karthik <vsskartheek@gmail.com>
2026-03-30 14:42:55 +0530

    Update and rename test_kmeans_elbow.py to demo_kmeans_elbow.py

--- examples/demo_kmeans_elbow.py
+++ examples/demo_kmeans_elbow.py
@@ -1,13 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys, os
 
-# Route Python to your GlassBoxML root directory
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from datasets.generators import make_blobs
-# Import YOUR K-Means model!
-from models.kmeans2 import KMeansClustering 
+from glassboxml.datasets import make_blobs
+from glassboxml.models import KMeansClustering 
 def main():
     print("==================================================")
     print("      GlassBox-ML Utility: The Elbow Method       ")
