bc1e8792388c62d6aad878f54a219212438c0f4d
V SS Karthik <vsskartheek@gmail.com>
2026-05-22 16:30:45 +0530

    Add Pipeline import to __init__.py

--- glassboxml/__init__.py
+++ glassboxml/__init__.py
@@ -4,6 +4,8 @@ from .metrics import *
 from .diagnostics import *
 from .preprocessing import *
 from .tuning import *
+from .feature_extraction import *
 
 from .core.model_selection import train_test_split
 from .core.optimizer import Momentum
+from .core.pipeline import Pipeline
