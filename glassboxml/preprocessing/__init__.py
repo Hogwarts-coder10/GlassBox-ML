ae6e0cc7ab9dede6bbf3ddf5c7336d278dea47dd
VSSKarthik <vsskartheek@gmail.com>
2026-07-19 15:07:13 +0530

    Added PolyNomialFeatures class in __init__.py

--- glassboxml/preprocessing/__init__.py
+++ glassboxml/preprocessing/__init__.py
@@ -1,6 +1,4 @@
-from ._standard_scaler import StandardScaler
 from ._label import LabelEncoder
-__all__ = [
-    "StandardScaler",
-    "LabelEncoder"
-]
+from ._standard_scaler import PolynomialFeatures, StandardScaler
+
+__all__ = ["StandardScaler", "LabelEncoder", "PolynomialFeatures"]
